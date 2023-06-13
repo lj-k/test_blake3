2913c31812a1b27d4d3659336c9e787d3ab91192
lj.k <lj.k@outlook.com>
2023-06-13 22:05:15 +0800

    add time result without anything in module programs

--- src/blake3_dispatch_empty.c
+++ src/blake3_dispatch_empty.c
@@ -25,14 +25,14 @@ void blake3_compress_in_place(uint32_t cv[8],
                               const uint8_t block[BLAKE3_BLOCK_LEN],
                               uint8_t block_len, uint64_t counter,
                               uint8_t flags) {
-    cv[0] *= block[2] + block_len + counter + flags;
+    //cv[0] *= block[2] + block_len + counter + flags;
 }
 
 void blake3_compress_xof(const uint32_t cv[8],
                          const uint8_t block[BLAKE3_BLOCK_LEN],
                          uint8_t block_len, uint64_t counter, uint8_t flags,
                          uint8_t out[64]) {
-    out[2]=cv[0] +block[2] + block_len + counter + flags;
+    //out[2]=cv[0] +block[2] + block_len + counter + flags;
 
 }
 
@@ -40,11 +40,11 @@ void blake3_hash_many(const uint8_t *const *inputs, size_t num_inputs,
                       size_t blocks, const uint32_t key[8], uint64_t counter,
                       bool increment_counter, uint8_t flags,
                       uint8_t flags_start, uint8_t flags_end, uint8_t *out) {
-    out[2] = inputs[0]  + blocks + counter + flags+ key[1];
+    //out[2] = inputs[0]  + blocks + counter + flags+ key[1];
 
 }
 
 size_t blake3_simd_degree(void) {
-    return 1;
+    return 16;
 }
 
